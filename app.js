90a6c7aeb87cb2ff57c22bcc80d7a246a974ddef
BryceForstie <forstiebryce@gmail.com>
2025-01-26 14:32:34 -0800

    Update app.js
    started working through the logic for the text input. App adds the text to an array (toDoList) upon clicking 'add' button. also removes text on 'add' click, and refresh.

--- app.js
+++ app.js
@@ -9,18 +9,41 @@ const addTaskBtn = document.querySelector('#addTaskBtn');
 const removeTaskBtn = document.querySelector('#removeTaskBtn');
 const sortTaskBtn = document.querySelector('#sortTaskBtn');
 
-// function to console.log the text entered into the task input.
+//variable to capture the text inputed into the text input. Trying to see if its better to create the var here and call it later, or create it within functions. *disabling. this causes more issues 
+// let textValue = taskInput.value;
 
-const returnInputValue = () => {
-    let inputText = taskInput.value;
-    console.log(inputText);
-}
+//empty array, this is the wrapper for my to do list
+const toDoList = [];
 
-// return the returnInputValue function when clicking the add button. The function currently should just console.log whatever is typed in when you click add
+//function that takes the text inside the text input, and add it to the end of the to do list array.
+const addTask = ()=> {
+    let textValue = taskInput.value;
+    toDoList.push(textValue);
+};
 
-addTaskBtn.addEventListener('click', returnInputValue);
+//need to figure out how we are going to create and display the tasks...
+//function that should console.log each item in the array
+const logTasks = ()=>{
+    toDoList.forEach((el) => console.log(el));
+};
 
-//create an h2 below the To do list h1
+
+
+//function to clear the text input field
+const clearText = ()=> {
+    taskInput.value = '';
+};
+
+//function or functions to sort alphabetically 
+
+//adds the text in the text input to the array
+addTaskBtn.addEventListener('click', addTask);
+//clears text input on task add
+addTaskBtn.addEventListener('click', clearText);
+//clears text input on load/refresh
+window.addEventListener('load', clearText);
+//execute console log array function on add btn click
+addTaskBtn.addEventListener('click', logTasks);
 
 
 
