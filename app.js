f2bf7af55562fef298888214f58cbe33969e4c6a
BryceForstie <forstiebryce@gmail.com>
2025-01-30 16:56:02 -0800

    Update app.js

--- app.js
+++ app.js
@@ -35,26 +35,55 @@ const addTask = ()=> {
     }
 };
 
+//function to store task element creations, and also append them to the todolist section
 const displayTask = ()=> {
         for (let i = 0; i < toDoList.length+1; i++) {
             if (taskInput.value === '') {
                 taskInput.placeholder = 'Cannot be blank!'
             } else if (i === toDoList.length) {
+        //variable that stores div creation
         const newTaskDisplay = document.createElement('div');
-        newTaskDisplay.id = `display${i}`
+        newTaskDisplay.classList.add('taskDiv');
+        newTaskDisplay.id = `taskDiv${i}`;
+        //variable that store checkbox creation
         const displayCheckBox = document.createElement('input');
+        displayCheckBox.classList.add('checkboxTask');
         displayCheckBox.type = 'checkbox';
         displayCheckBox.id = `checkboxTask${i}`
+        //variabe that stores label creation
         const displayText = document.createElement('label');
         displayText.id = `taskText${i}`
         displayText.htmlFor = `checkboxTask${i}`; 
         displayText.textContent = toDoList[i-1].taskText;
+        //actually adding the classes now
         newTaskDisplay.appendChild(displayCheckBox);
         newTaskDisplay.appendChild(displayText);
         taskList.appendChild(newTaskDisplay);
         };
     };
 };
+
+//stores all checkboxes in a variable for later use 
+const checkboxs = document.getElementsByClassName('checkboxTask');
+
+//function to remove checked tasks
+const removeTask = () => {
+    for (let i = 0; i<toDoList.length; i++) {
+        if (document.querySelector(`#checkboxTask${i+1}`).checked) {
+            document.querySelector(`#taskDiv${i+1}`).remove();
+        }
+    }
+};
+
+//function to re-write ids so above code works
+const rewriteId = () => {
+    for (let i = 0; i<toDoList.length; i++) {
+        newTaskDisplay.id = `taskDiv${i}`;
+        displayCheckBox.id = `checkboxTask${i}`;
+        displayText.id = `taskText${i}`;
+        displayText.htmlFor = `checkboxTask${i}`; 
+    }
+}
 
 // function to clear the text input field
 const clearText = ()=> {
@@ -69,6 +98,10 @@ window.addEventListener('load', clearText);
 addTaskBtn.addEventListener('click', displayTask);
 // clears text input on task add
 addTaskBtn.addEventListener('click', clearText);
+// removes the checked tasks
+removeTaskBtn.addEventListener('click', removeTask);
+// rewrites the ids of a existing elements on removeTask click
+removeTaskBtn.addEventListener('click', rewriteId);
 
 
 
