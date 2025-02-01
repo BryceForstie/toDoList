b80a2e5c3b3bd4583cd7ff9a29ae7a72c177317f
BryceForstie <forstiebryce@gmail.com>
2025-02-01 10:38:24 -0800

    update
    Finally figured out how to removed tasks (i dont know if its effective but it works)
    
    basically, I store items I dont want to delete in a new array, wipe my old array and push items from new array into OG array, then delete all divs and create new ones with the re-written array.

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ const updatedToDoList = [];
 const allTaskDisplays = document.querySelectorAll('div');
 // variable that selects all checkboxes
 const allCheckBoxes = document.querySelectorAll('.checkboxTask')
+// stores number of checked checkboxes in a var
+let numOfChecked = 0;
 
 //add an object to array
 function addTask() {
@@ -71,10 +73,27 @@ const clearText = ()=> {
     taskInput.value = '';
 };
 
+// function to count num of checked items
+function countChecked() {
+    for (let i = 0; i<toDoList.length; i++) {
+        if (document.querySelector(`#checkboxTask${i+1}`).checked) {
+            numOfChecked++;
+        };
+    };
+};
+
+//function to clear numOfChecked 
+function wipeNumOfChecked() {
+    numOfChecked = 0;
+};
+
 // creates a new array with the values of tasks that are not checked
 function createUpdatedArray() {
     for (let i = 0; i<toDoList.length; i++) {
-        if (document.querySelector(`#checkboxTask${i+1}`).checked === false) {
+        // I dont want this to run if nothing is checked
+        if (numOfChecked === 0) {
+            taskInput.placeholder = 'No Checked Items';
+        } else if (document.querySelector(`#checkboxTask${i+1}`).checked === false) {
             updatedToDoList.push(toDoList[i]);
         };
     };
@@ -82,44 +101,67 @@ function createUpdatedArray() {
 
 // removes everything from the toDoList array
 function wipeToDoList() {
+     // I dont want this to run if nothing is checked
+     if (numOfChecked === 0) {
+        taskInput.placeholder = 'No Checked Items';
+    } else {
     toDoList.length = 0;
+    }
 };
 
 // updates toDoList array with values in the new array I created with unchecked values
 function updateToDoList() {
+     // I dont want this to run if nothing is checked
+     if (numOfChecked === 0) {
+        taskInput.placeholder = 'No Checked Items';
+    } else {
     for (let i = 0; i<updatedToDoList.length; i++) {
         toDoList.push(updatedToDoList[i])
+        };
     };
 };
 
 // removes everything from the updatedToDoList
 function wipeUpdatedToDoList() {
+     // I dont want this to run if nothing is checked
+     if (numOfChecked === 0) {
+        taskInput.placeholder = 'No Checked Items';
+    } else {
     updatedToDoList.length = 0;
+    };
 };
 
 // removes all divs on page
 function removeTaskDisplays() {
+     // I dont want this to run if nothing is checked
+     if (numOfChecked === 0) {
+        taskInput.placeholder = 'No Checked Items';
+    } else {
     taskList.innerHTML = '';
+    };
 };
 
 // adds new task displays with the updated toDoList
 function updatedDisplayTask() {
-    for (let i = 0; i < toDoList.length + 1; i++) {
-        if (i === toDoList.length) {
+     // I dont want this to run if nothing is checked
+     if (numOfChecked === 0) {
+        taskInput.placeholder = 'No Checked Items';
+    } else {
+    for (let i = 0; i < toDoList.length; i++) {
             //variable that stores div creation
             const newTaskDisplay = document.createElement('div');
             newTaskDisplay.classList.add('taskDiv');
-            newTaskDisplay.id = `taskDiv${i}`;
+            newTaskDisplay.id = `taskDiv${i+1}`;
             //variable that store checkbox creation
             const displayCheckBox = document.createElement('input');
             displayCheckBox.classList.add('checkboxTask');
             displayCheckBox.type = 'checkbox';
-            displayCheckBox.id = `checkboxTask${i}`;
+            displayCheckBox.id = `checkboxTask${i+1}`;
             //variabe that stores label creation
             const displayText = document.createElement('label');
-            displayText.id = `taskText${i}`;
-            displayText.htmlFor = `checkboxTask${i}`;
-            displayText.textContent = toDoList[i - 1];
+            displayText.id = `taskText${i+1}`;
+            displayText.htmlFor = `checkboxTask${i+1}`;
+            displayText.textContent = toDoList[i];
             //actually adding the classes now
             newTaskDisplay.appendChild(displayCheckBox);
             newTaskDisplay.appendChild(displayText);
@@ -128,14 +170,12 @@ function updatedDisplayTask() {
     };
 };
 
-// checkbox thing
-function checkBoxThing() {
-    if (this.checked) {
-        console.log('checked');
-    } else {
-        console.log('unchcked');
-    }
-}
+// updates the placeholder back to default
+function updatePlaceHolder() {
+    if(numOfChecked>0) {
+    taskInput.placeholder = 'enter a task';
+    };
+};
 
 // adds the text in the text input to the array
 addTaskBtn.addEventListener('click', addTask);
@@ -145,6 +185,8 @@ window.addEventListener('load', clearText);
 addTaskBtn.addEventListener('click', displayTask);
 // clears text input on task add
 addTaskBtn.addEventListener('click', clearText);
+// counts # of checked items when remove button is clicked 
+removeTaskBtn.addEventListener('click', countChecked);
 // add the unchecked items of the toDoList to a new array
 removeTaskBtn.addEventListener('click', createUpdatedArray);
 // wipes toDoList after creating the second updated array 
@@ -157,8 +199,14 @@ removeTaskBtn.addEventListener('click', wipeUpdatedToDoList);
 removeTaskBtn.addEventListener('click', removeTaskDisplays);
 // adds new task displays with current array info 
 removeTaskBtn.addEventListener('click', updatedDisplayTask);
-// checkbox thing
-allCheckBoxes.addEventListener('change', checkBoxThing);
+// updates placeholder back to default after removing items (only happens if items are removed)
+removeTaskBtn.addEventListener('click', updatePlaceHolder);
+// changes numOfChecked back to 0 after everything else that happens on remove btn click
+removeTaskBtn.addEventListener('click', wipeNumOfChecked);
+
+
+
+
 
 
 
